4068c775786e01874bc3d7f02ef2d61622ca0d13
Erne <edainesi@fi.uba.ar>
2021-11-19 21:42:25 -0300

    elimino sqlite3

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,17 +1,9 @@
-import { Request, Response } from "express";
-import { MongoDatabase } from "../database/mongodb/MongoDatabase";
-import { SqliteDatabase } from "../database/sqlite3/SqliteDatabase";
+import {Request, Response} from "express";
+import {MongoDatabase} from "../database/mongodb/MongoDatabase";
 
 const mongodb: MongoDatabase = new MongoDatabase();
-const sqlite: SqliteDatabase = new SqliteDatabase();
 
-try {
-	sqlite.createProductsTable();
-} catch (err) {
-	console.error(err);
-}
-
-export const getProductsPage =  (req: Request, res: Response) => {
+export const getProductsPage = (req: Request, res: Response) => {
 	res.render('pages/productos');
 };
 
@@ -32,18 +24,16 @@ export const getProduct = (req: Request, res: Response) => {
 	const idProduct: string = req.params.id;
 	try {
 		const productMongo = mongodb.getProduct(idProduct, null);
-		const productSqlite = sqlite.getProduct(idProduct);
-		res.json({productMongo, productSqlite});
+		res.json({productMongo});
 	} catch (err) {
 		res.status(404).json({error: 'product not found'});
 	}
 };
 
-export const saveProduct = (req: Request, res: Response) => { 
+export const saveProduct = (req: Request, res: Response) => {
 	const product = {...req.body};
 	try {
 		mongodb.insertProduct(product);
-		sqlite.insertProduct(product);
 		res.redirect('/products');
 	} catch (err) {
 		res.status(500).json({error: "couldn't save product"});
@@ -54,7 +44,6 @@ export const updateProduct = (req: Request, res: Response) => {
 	try {
 		const id: string = req.params.productId;
 		const updatedProduct = mongodb.updateProduct(id, req.body);
-		sqlite.updateProduct(id, req.body);
 		res.send(updatedProduct);
 	} catch (err) {
 		res.status(500).json({error: "couldn't update product"});
@@ -65,7 +54,6 @@ export const deleteProduct = (req: Request, res: Response) => {
 	try {
 		const productId: string = req.params.productId;
 		const deletedProduct = mongodb.deleteProduct(productId);
-		sqlite.deleteProduct(productId);
 		res.send(deletedProduct);
 	} catch (err) {
 		res.status(500).json({error: "couldn't delete product"});
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,9 @@ import productsController from './routes/products.route';
 import cartController from './routes/cart.route';
 import {PORT} from './constants';
 import {MongoDatabase} from './database/mongodb/MongoDatabase';
-import {SqliteDatabase} from './database/sqlite3/SqliteDatabase';
 
 const app: Application = express();
 const ejs = require("ejs").__express; // solucion a error "cannot find ejs module"
-const sqlite = new SqliteDatabase();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -23,7 +21,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 MongoDatabase.connect();
-//sqlite.createProductsTable();
 
 app.listen(PORT, () => {
 	console.log(`Servidor express Typescript/Webpack en puerto ${PORT}`);
