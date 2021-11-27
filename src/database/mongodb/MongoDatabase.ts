6e2c5fa3a192cee60b48112e66355afcaa2bc5b5
Erne <edainesi@fi.uba.ar>
2021-11-27 15:33:56 -0300

    borro import innecesario

--- src/database/mongodb/MongoDatabase.ts
+++ src/database/mongodb/MongoDatabase.ts
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import productsSchema, { IProduct } from '../../schemas/mongodb/productsSchema';
 import {
     MONGODB_URL,
