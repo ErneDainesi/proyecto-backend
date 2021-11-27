d750bbb258c5d0143f233e6abd3217906eb4ca68
Erne <edainesi@fi.uba.ar>
2021-11-27 15:34:23 -0300

    refactorizo schema de productos

--- src/schemas/mongodb/productsSchema.ts
+++ src/schemas/mongodb/productsSchema.ts
@@ -1,20 +1,30 @@
 import {Document, model, Schema} from "mongoose";
 
 export interface IProduct extends Document {
-    name: string,
-    description: string,
-    stock: number,
-    price: number
+	name: string,
+	description: string,
+	stock: number,
+	price: number
 }
 
 const productSchema = new Schema<IProduct>({
-    name: {
-        type: String,
-        required: true
-    },
-    description: String,
-    stock: Number,
-    price: Number
+	name: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	stock: {
+		type: Number,
+		required: true
+	},
+	price: {
+		type: Number,
+		required: true
+	}
 });
 
-export default model<IProduct>("ProductMongo", productSchema);
+export default model<IProduct>("Product", productSchema);
+
