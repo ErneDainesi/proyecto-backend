bfd90bf7a9611df97a84e107fdc1c97f8e7cbd96
Erne <edainesi@fi.uba.ar>
2021-12-08 19:11:25 -0300

    agrego credenciales de twilio

--- src/twilio.ts
+++ src/twilio.ts
@@ -1,4 +1,9 @@
-import client from 'twilio';
+import twilio from 'twilio';
 
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
+
+const client = twilio(accountSid, authToken);
+
+export default client;
+
