9f9b0c07dd69b8a826e8196cdf1018b89bc92cd3
Erne <edainesi@fi.uba.ar>
2021-12-21 19:23:09 -0300

    borro metodo de conexion a mongo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import { Request, Response } from 'express';
-import { PORT } from './constants';
-import { MongoDatabase } from './database/mongodb/MongoDatabase';
-import { config } from "dotenv";
+import {Request, Response} from 'express';
+import {PORT} from './constants';
+import {config} from "dotenv";
 import app from './app';
 import logger from './logger/winston';
 
-config(); // config para variables de entorno
-MongoDatabase.connect();
+config();
 
 app.get('/', (req: Request, res: Response) => {
-  res.render('pages/index');
+	res.render('pages/index');
 });
 
 app.listen(PORT, () => {
-  logger.info(`Express Server listening on port: ${PORT}`);
+	logger.info(`Express Server listening on port: ${PORT}`);
 });
 
