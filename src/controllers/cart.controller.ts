7697c349e9dfe1f4825173d42e20dfda2f1b3002
Erne <edainesi@fi.uba.ar>
2021-12-28 13:05:05 -0300

    saco persistencia en archivo de cart

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,9 +1,7 @@
-import {Cart} from '../lib/Cart';
 import {Request, Response} from 'express';
-import {overwriteFile} from '../lib/utilities';
+import {Cart} from '../models/Cart';
 
 const carts: Cart[] = [];
-const FILE_NAME = "./carts.json";
 
 export const creatNewCart = (req: Request, res: Response) => {
 	const cart: Cart = new Cart(carts.length + 1, Date.now());
@@ -30,7 +28,6 @@ export const saveProduct = (req: Request, res: Response) => {
 	};
 	const cart: Cart = carts[+req.params.idCart - 1];
 	cart.addToCart(product);
-	overwriteFile(FILE_NAME, carts);
 	res.json(product);
 };
 
@@ -38,6 +35,5 @@ export const deleteProduct = (req: Request, res: Response) => {
 	const productId: number = +req.params.idProduct;
 	const cart: Cart = carts[+req.params.idCart - 1];
 	const deletedProduct: Object = cart.removeFromCart(productId);
-	overwriteFile(FILE_NAME, carts);
 	res.send(deletedProduct);
 };
