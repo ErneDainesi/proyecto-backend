0d3f368c5935211e54f70197ffbd3441327837c9
Erne <edainesi@fi.uba.ar>
2022-01-09 17:36:53 -0300

    refactor code and fix errors with next function

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -1,27 +1,24 @@
-import {ErrorRequestHandler} from 'express';
+import {Request, Response, NextFunction} from 'express';
+import logger from '../logger/winston';
 
-// Declaro el tipo User
-interface User {
-	id: number,
-	isAdmin: boolean
-};
-
-// Array modelando la base de datos de usuarios
-const users: Array<User> = [
-	{id: 1, isAdmin: true},
-	{id: 2, isAdmin: false}
-];
-
-// Busca al usuario por su id (que es pasado como parametro en el request)
-// dentro del array "users" y valida que este sea administrador
-export const checkIfIsAdmin: ErrorRequestHandler = (err, req, res, next) => {
-	const userId: number = +req.params.userId - 1;
-	const user: User = users[userId];
-	if (!user.isAdmin) {
-		res.status(400).json({
-			error: -1,
-			description: `Route ${req.originalUrl} method ${req.method} not authorized`
-		});
+export const checkIfIsAdmin = (req: Request, res: Response, next: NextFunction) => {
+	const isAdmin: boolean = !!req.session.user?.isAdmin;
+	if (isAdmin) {
+        next();
+        return;
 	}
-	next();
+    res.status(400).json({
+        error: -1,
+        description: `Route ${req.originalUrl} method ${req.method} not authorized`
+    });
 };
+
+export const checkIfSessionIsActive = (req: Request, res: Response, next: NextFunction) => {
+    if (req.session.user) {
+        next();
+        return;
+    }
+    logger.error('User not logged in');
+    res.redirect('/login');
+}
+
